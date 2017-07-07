62de8ac94b0b4ae76fc08556a18747bea81b0bc2
Jieke Pan <jpan@mobiquityinc.com>
2017-07-07 18:21:21 +0200

    Updated Speechlet

--- alexa-skills-backbase/src/main/java/alexa/skills/backbase/MyBankSpeechlet.java
+++ alexa-skills-backbase/src/main/java/alexa/skills/backbase/MyBankSpeechlet.java
@@ -4,11 +4,18 @@ import alexa.skills.backbase.lib.RestClient;
 import com.amazon.speech.json.SpeechletRequestEnvelope;
 import com.amazon.speech.slu.Intent;
 import com.amazon.speech.speechlet.*;
+import com.amazon.speech.speechlet.interfaces.audioplayer.AudioItem;
+import com.amazon.speech.speechlet.interfaces.audioplayer.PlayBehavior;
+import com.amazon.speech.speechlet.interfaces.audioplayer.Stream;
+import com.amazon.speech.speechlet.interfaces.audioplayer.directive.PlayDirective;
 import com.amazon.speech.ui.PlainTextOutputSpeech;
 import com.amazon.speech.ui.Reprompt;
 import com.amazon.speech.ui.SimpleCard;
+import com.amazon.speech.ui.SsmlOutputSpeech;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
 
 public class MyBankSpeechlet implements SpeechletV2 {
 
@@ -17,22 +24,17 @@ public class MyBankSpeechlet implements SpeechletV2 {
     private static final String MY_BANK_CART = "MyBank";
     private static final String STOP_INTENT = "AMAZON.StopIntent";
     private static final String NO_INTENT = "AMAZON.NoIntent";
-
     private static final String YES_INTENT = "AMAZON.YesIntent";
+
     private static final String STATUS_INTENT = "GetStatus";
-    private static final String PAYMENT_REQUEST_INTENT = "PaymentRequest";
     private static final String TRANSFER_INTENT = "Transfer";
-    private static final String CREDIT_CONFIRMATION_INTENT = "CreditConfirmation";
+    private static final String FIRST_TIME_TRANSFER = "FirstTimeTransfer";
 
-    private static final String PERSON_NAME_SLOT = "PersonName";
-    private static final String AMOUNT_SLOT = "Amount";
-    private static final String MONEY_SLOT = "Money";
-    private static final String CREDIT_SLOT = "CreditName";
-
-    private static final String WELCOME_MESSAGE = "Hi %s, Welcome to My Bank.";
+    private static final String WELCOME_MESSAGE = "Hi Rik, I hope everything is alright, you seem to be short on funds lately. Anything I should know about?";
     private static final String GOODBYE_MESSAGE = "Thank you and enjoy your day.";
     private static final String ACCOUNT_BALANCE_MESSAGE = "You have 1000 euros in your debit account.";
-    private static final String PAYMENT_REQUEST_MESSAGE = "The payment has been sent to %s";
+    private static final String TRANSFER_MESSAGE = "okay done, don't forget you still owe Lisa 20 euros for those drinks last Friday";
+    private static final String ANOTHER_TRANSFER_MESSAGE = "done, your balance is now 450 euro's. Anything else I can do for you today?";
     private static final String UNKNOWN_MESSAGE = "Sorry, I could not understand your question.";
 
     @Override
@@ -61,21 +63,22 @@ public class MyBankSpeechlet implements SpeechletV2 {
 
         Intent intent = request.getIntent();
         String intentName = (intent != null) ? intent.getName() : new String();
+        Session session = speechletRequestEnvelope.getSession();
+
         switch (intentName) {
             case STOP_INTENT:
             case NO_INTENT:
-            case YES_INTENT:
                 return getGoodByeResponse();
+            case YES_INTENT:
+                return sendTransferRequest(ANOTHER_TRANSFER_MESSAGE, session);
             case STATUS_INTENT:
                 return getStatusResponse();
-            case PAYMENT_REQUEST_INTENT: {
-                return sendPaymentRequest(intent.getSlot(PERSON_NAME_SLOT).getValue());
-            }
             case TRANSFER_INTENT: {
-                return sendTransferRequest(intent.getSlot(AMOUNT_SLOT).getValue(), intent.getSlot(MONEY_SLOT).getValue());
-            }
-            case CREDIT_CONFIRMATION_INTENT: {
-                return getCreditConfirmation(intent.getSlot(CREDIT_SLOT).getValue());
+                if ("true".equals(session.getAttribute(FIRST_TIME_TRANSFER))) {
+                    return sendTransferRequest(TRANSFER_MESSAGE, session);
+                } else {
+                    return sendAnotherTransferRequest(ANOTHER_TRANSFER_MESSAGE, session);
+                }
             }
             default:
                 return getUnknownCommandResponse();
@@ -90,20 +93,7 @@ public class MyBankSpeechlet implements SpeechletV2 {
     }
 
     private SpeechletResponse getWelcomeResponse() {
-        String userName = null;
-
-        try {
-            String token = restClient.login();
-            userName = restClient.getUserName(token);
-            if (userName == null || userName.isEmpty()) {
-                userName = "";
-            }
-        } catch (Exception e) {
-            userName = "";
-        }
-
-
-        return createSimpleResponse(String.format(WELCOME_MESSAGE, userName));
+        return createRepromptResponse(WELCOME_MESSAGE, "");
     }
 
     private SpeechletResponse getGoodByeResponse() {
@@ -111,22 +101,17 @@ public class MyBankSpeechlet implements SpeechletV2 {
     }
 
     private SpeechletResponse getStatusResponse() {
-        return createSimpleResponse(ACCOUNT_BALANCE_MESSAGE);
+        return createSimpleSsmlResponse(ACCOUNT_BALANCE_MESSAGE);
     }
 
-    private SpeechletResponse sendPaymentRequest(String contact) {
-        String message = String.format(PAYMENT_REQUEST_MESSAGE, contact);
-        return createSimpleResponse(message);
+    private SpeechletResponse sendTransferRequest(String speechText, Session session) {
+        session.setAttribute(FIRST_TIME_TRANSFER, "true");
+        return createRepromptResponse(speechText, "");
     }
 
-    private SpeechletResponse sendTransferRequest(String amount, String money) {
-        String message = String.format("%s %s has been transferred.", amount, money);
-        return createSimpleResponse(message);
-    }
-
-    private SpeechletResponse getCreditConfirmation(String creditName) {
-        String message = String.format("%s has been transferred.", creditName);
-        return createSimpleResponse(message);
+    private SpeechletResponse sendAnotherTransferRequest(String speechText, Session session) {
+        session.setAttribute(FIRST_TIME_TRANSFER, "false");
+        return createRepromptResponse(speechText, "");
     }
 
     private SpeechletResponse getUnknownCommandResponse() {
@@ -151,6 +136,32 @@ public class MyBankSpeechlet implements SpeechletV2 {
         reprompt.setOutputSpeech(repromptSpeech);
 
         return SpeechletResponse.newAskResponse(speech, reprompt, card);
+    }
+
+    private SpeechletResponse createSimpleSsmlResponse(String speechText) {
+        SsmlOutputSpeech output = new SsmlOutputSpeech();
+        output.setSsml("<speak> " + speechText + "<audio src='https://s3-eu-west-1.amazonaws.com/tempmediafile/alexa.mp3'/> " + " </speak>");
+
+        return SpeechletResponse.newTellResponse(output, createCard(MY_BANK_CART, speechText));
+    }
+
+    private SpeechletResponse createSimpleDirectiveResponse(String speechText, String url) {
+        SimpleCard card = createCard(MY_BANK_CART, speechText);
+        PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
+        speech.setText(speechText);
+
+        PlayDirective directive = new PlayDirective();
+        AudioItem audioItem = new AudioItem();
+        Stream stream = new Stream();
+        stream.setUrl(url);
+        audioItem.setStream(stream);
+        directive.setAudioItem(audioItem);
+        directive.setPlayBehavior(PlayBehavior.REPLACE_ALL);
+
+        SpeechletResponse response = SpeechletResponse.newTellResponse(speech, card);
+        response.setDirectives(Arrays.asList(directive));
+
+        return response;
     }
 
     private SimpleCard createCard(String title, String speechText) {
